b0dddf001794c1990334f7a939b6b84eff21bd75
Bronley Plumb <bronley@gmail.com>
2024-10-17 09:19:56 -0400

    Fix plugin integration with lsp

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -1,7 +1,7 @@
 import * as fsExtra from 'fs-extra';
 import * as fastGlob from 'fast-glob';
-import type { BeforeFileTranspileEvent, BscFile, CompilerPlugin, Editor, Program, XmlFile } from 'brighterscript';
-import { AstEditor, createSGAttribute, isBrsFile, isXmlFile, standardizePath as s } from 'brighterscript';
+import type { BscFile, CompilerPlugin, Editor, Program, XmlFile } from 'brighterscript';
+import { AstEditor, createSGAttribute, isXmlFile, standardizePath as s, util } from 'brighterscript';
 import { SGField, SGFunction, SGInterface, SGScript } from 'brighterscript/dist/parser/SGTypes';
 const cwd = s`${__dirname}/../`;
 
@@ -10,15 +10,20 @@ export class Plugin implements CompilerPlugin {
 
 	afterProgramCreate(program: Program) {
 		//inject the the required library files at the start of the program so they're available to all files from the very start
-		const files = fastGlob.sync('**/*.{bs,brs,xml}', {
-			absolute: false,
-			cwd: s`${cwd}/lib`
-		});
+		const files = fastGlob
+			.sync('**/*.{bs,brs,xml}', {
+				absolute: false,
+				cwd: s`${cwd}/lib`
+			}).map(x => ({
+				src: s`${cwd}/lib/${x}`,
+				dest: x
+			}));
+
+		//add these to the program's files array so it works in lsp mode
+		program.options.files.push(...files);
+
 		for (const file of files) {
-			program.setFile({
-				src: s`${cwd}/lib/${file}`,
-				dest: file
-			}, fsExtra.readFileSync(s`${cwd}/lib/${file}`).toString());
+			program.setFile(file, fsExtra.readFileSync(s`${cwd}/lib/${file}`).toString());
 		}
 	}
 
@@ -27,6 +32,10 @@ export class Plugin implements CompilerPlugin {
 			//inject the reftracker.bs file as a reference
 			this.injectScriptAndCallfunc(file, new AstEditor());
 		}
+	}
+
+	beforeProgramValidate(program: Program) {
+		console.log('beforeProgramValidate');
 	}
 
 	// beforeFileTranspile(event: BeforeFileTranspileEvent) {
@@ -39,7 +48,6 @@ export class Plugin implements CompilerPlugin {
 	// }
 
 	private injectScriptAndCallfunc(file: XmlFile, editor: Editor) {
-		//inject the script tag
 		if (!file.ast.component?.api) {
 			file.ast.component!.api = new SGInterface();
 		}
@@ -51,7 +59,7 @@ export class Plugin implements CompilerPlugin {
 		editor.arrayPush(
 			file.ast.component!.api.functions,
 			new SGFunction(
-				{ text: 'function' },
+				{ text: 'function', range: util.createRange(0, 0, 0, 99) },
 				[
 					createSGAttribute('name', 'reftracker_internal_execute')
 				]
@@ -61,7 +69,7 @@ export class Plugin implements CompilerPlugin {
 		editor.arrayPush(
 			file.ast.component!.api.fields,
 			new SGField(
-				{ text: 'field' },
+				{ text: 'field', range: util.createRange(0, 0, 0, 99) },
 				[
 					createSGAttribute('id', 'reftrackerEnabled'),
 					createSGAttribute('type', 'boolean'),
@@ -74,12 +82,21 @@ export class Plugin implements CompilerPlugin {
 		editor.arrayPush(
 			file.ast.component!.scripts,
 			new SGScript(
-				{ text: 'script' },
+				{ text: 'script', range: util.createRange(0, 0, 0, 99) },
 				[
 					createSGAttribute('type', 'text/brightscript'),
 					createSGAttribute('uri', 'pkg:/source/reftrackerLib.bs')
 				]
 			)
 		);
+		// //for bsc v0, we also have to manipulate parser.references.scriptTagImports
+		// editor.arrayPush(
+		// 	file.parser.references.scriptTagImports,
+		// 	{
+		// 		pkgPath: s`source/reftrackerLib.bs`,
+		// 		text: 'pkg:/source/reftrackerLib.bs',
+		// 		filePathRange: util.createRange(0, 0, 0, 0)
+		// 	}
+		// );
 	}
 }
