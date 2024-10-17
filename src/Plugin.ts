e2b38810774ae735097e52f1207942720e957844
Bronley Plumb <bronley@gmail.com>
2024-10-16 23:30:16 -0400

    Add first take at building a full list of all refs

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -1,18 +1,25 @@
 import * as fsExtra from 'fs-extra';
+import * as fastGlob from 'fast-glob';
 import type { BeforeFileTranspileEvent, CompilerPlugin, Editor, Program, XmlFile } from 'brighterscript';
 import { createSGAttribute, isBrsFile, isXmlFile, standardizePath as s } from 'brighterscript';
-import { SGFunction, SGInterface, SGScript, SGTag } from 'brighterscript/dist/parser/SGTypes';
-const libPath = s`${__dirname}/../lib/source/reftracker.bs`;
+import { SGField, SGFunction, SGInterface, SGScript } from 'brighterscript/dist/parser/SGTypes';
+const cwd = s`${__dirname}/../`;
 
 export class Plugin implements CompilerPlugin {
 	name = 'bsc-plugin-reftracker';
 
 	afterProgramCreate(program: Program) {
-		//inject the library file at the start of the program so it's available to all files from the very start
-		program.setFile({
-			src: libPath,
-			dest: 'source/reftracker.bs'
-		}, fsExtra.readFileSync(libPath).toString());
+		//inject the the required library files at the start of the program so they're available to all files from the very start
+		const files = fastGlob.sync('**/*.{bs,brs,xml}', {
+			absolute: false,
+			cwd: s`${cwd}/lib`
+		});
+		for (const file of files) {
+			program.setFile({
+				src: s`${cwd}/lib/${file}`,
+				dest: file
+			}, fsExtra.readFileSync(s`${cwd}/lib/${file}`).toString());
+		}
 	}
 
 	beforeFileTranspile(event: BeforeFileTranspileEvent) {
@@ -33,12 +40,25 @@ export class Plugin implements CompilerPlugin {
 			file.ast.component!.api.functions = [];
 		}
 
+		//add the interface function
 		editor.arrayPush(
 			file.ast.component!.api.functions,
 			new SGFunction(
 				{ text: 'function' },
 				[
-					createSGAttribute('name', 'reftracker_execute')
+					createSGAttribute('name', 'reftracker_internal_execute')
+				]
+			)
+		);
+		//add a new interface field indicating this function is available
+		editor.arrayPush(
+			file.ast.component!.api.fields,
+			new SGField(
+				{ text: 'field' },
+				[
+					createSGAttribute('id', 'reftrackerEnabled'),
+					createSGAttribute('type', 'boolean'),
+					createSGAttribute('value', 'true')
 				]
 			)
 		);
@@ -50,7 +70,7 @@ export class Plugin implements CompilerPlugin {
 				{ text: 'script' },
 				[
 					createSGAttribute('type', 'text/brightscript'),
-					createSGAttribute('uri', 'pkg:/source/reftracker.bs')
+					createSGAttribute('uri', 'pkg:/source/reftrackerLib.bs')
 				]
 			)
 		);
